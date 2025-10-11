f2733d4ee6e0a8fd7b2ff44260c4cfd2a3cd81c1
aditya-niranjan <adityaniranjan57@gmail.com>
2025-10-11 23:45:48 +0530

    Insertion Sort

---
+++ SORTING-ALGORITHMS/INSERTION-SORT/Insertion-Sort.py
@@ -0,0 +1,24 @@
+
+
+
+
+# [3,5,6,4,8,9,10,7,1]
+
+def InsertionSort(nums):
+    n = len(nums)
+
+    for i in range(1,n):
+        key = nums[i]
+        j = i-1
+        while j>=0 and nums[j]>key:
+            nums[j+1] = nums[j]
+            j-=1
+
+        nums[j+1]=key
+
+    return nums
+
+
+
+print(InsertionSort([3,5,6,4,8,9,10,7,1]))
+
