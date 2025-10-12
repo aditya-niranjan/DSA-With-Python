60767e783ea15e888c5ee606b786601b552e524f
aditya-niranjan <adityaniranjan57@gmail.com>
2025-10-12 23:23:22 +0530

    Solved intersection multiple arrays

---
+++ Best-uniqueProblems/intersection_multiple_arrays.py.py
@@ -0,0 +1,15 @@
+
+
+nums = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+
+answer = []
+for num in (nums[0]):
+    if num in answer:
+        continue
+    
+
+  
+    for arr in nums[0:]:
+        if num == arr:
+             print(num,end=" ")
