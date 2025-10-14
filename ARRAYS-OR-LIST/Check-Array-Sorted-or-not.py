1ef56cc5f6e71e4ebe00aa743e2b95bfcf22e1a9
aditya-niranjan <adityaniranjan57@gmail.com>
2025-10-14 13:51:48 +0530

    Question -3

---
+++ ARRAYS-OR-LIST/Check-Array-Sorted-or-not.py
@@ -0,0 +1,15 @@
+# nums = [55,34,88,99,12,99,23,67,3]
+# nums = [1,2,3,4,5,6,7,8,9]
+
+def Sorted_or_not(nums):
+    n = len(nums)
+    j = 0
+    for i in range(0,n):
+        j = i+1
+        if nums[i] > j:
+            return "not sorted"
+        else:
+            return "sorted"
+      
+
+print(Sorted_or_not([1,2,3,4,5,6,7,8,9]))
