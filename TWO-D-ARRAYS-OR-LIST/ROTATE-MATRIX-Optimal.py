61259ce40f64f36b00f072eb957add9245b9186d
aditya-niranjan <adityaniranjan57@gmail.com>
2025-10-29 20:32:46 +0530

    solved the Rotate matrix 90 degree

--- TWO-D-ARRAYS-OR-LIST/ROTATE-MATRIX-Optimal.py
+++ TWO-D-ARRAYS-OR-LIST/ROTATE-MATRIX-Optimal.py
@@ -1,5 +1,4 @@
 
-
 matrix = [
   [1,2,3,4],
   [5,6,7,8],
@@ -7,7 +6,23 @@ matrix = [
   [13,14,15,16]
 ]
 
+for i in range(0,len(matrix)):
+    for j in range(0,len(matrix[0])):
+       print(matrix[i][j],end="  ")
+    print()
+
+print()
 n = len(matrix)
 
 for i in range(0,n-1):
-  
+  for j in range(i+1,n):
+    matrix[i][j],matrix[j][i]=matrix[j][i],matrix[i][j]
+
+for i in range(0,n):
+  matrix[i].reverse()
+
+for i in range(0,len(matrix)):
+    for j in range(0,len(matrix[0])):
+       print(matrix[i][j],end="  ")
+    print()
+
