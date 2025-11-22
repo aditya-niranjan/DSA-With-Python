5351e11f868d10c33a9a9223af2ca43b375e9629
aditya-niranjan <adityaniranjan57@gmail.com>
2025-11-22 22:25:37 +0530

    dll Created and travseed

--- OOPS-IN-PYTHON/LINKED-LIST/DOUBLY-LINKED-LIST/CREATING-DLL.py
+++ OOPS-IN-PYTHON/LINKED-LIST/DOUBLY-LINKED-LIST/CREATING-DLL.py
@@ -1,1 +1,43 @@
-print("hello world")
+
+
+class Node:
+    def __init__(self, val):
+        self.val =val 
+        self.next = None
+        self.prev = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_at_head(self, val):
+        new_node = Node(val)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+    def insert_at_tail(self, val):
+        new_node = Node(val)
+        if self.tail is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.prev = self.tail
+            self.tail.next = new_node
+            self.tail = new_node
+    def display(self):
+        current = self.head
+        while current:
+            print(current.val, end=" <-> ")
+            current = current.next
+        print("None")
+# Example usage:
+dll = DoublyLinkedList()
+dll.insert_at_head(10)
+dll.insert_at_head(20)
+dll.insert_at_tail(30)
+dll.display()
