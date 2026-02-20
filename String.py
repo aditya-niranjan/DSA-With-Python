29a79bdf124c2daa5a2065aed506d1d9b6856405
aditya-niranjan <adityaniranjan57@gmail.com>
2026-02-20 22:17:52 +0530

    string problem solved

---
+++ String.py
@@ -0,0 +1,17 @@
+string = "abcd"
+target = "abce"
+
+def compare_strings(string :str,target :str)->str:
+    if len(string) <=0:
+      return target
+    
+    for i, char in enumerate(string):
+        if char == target[i]:
+            continue
+        else: 
+            return target[i]
+
+result = compare_strings(string,target)
+print(result)
+
+# time complexity: O(n) where n is the length of the string
