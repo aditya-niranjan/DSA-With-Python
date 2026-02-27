1cbb720b22a9ca43ba124da7458ccc330639a4f9
aditya-niranjan <adityaniranjan57@gmail.com>
2026-02-27 20:05:49 +0530

    added the join split and lot understanded

---
+++ 2026-Practice/5.py
@@ -0,0 +1,10 @@
+
+# direct link to the slicing and join and split web site 
+# https://www.geeksforgeeks.org/python/how-to-substring-a-string-in-python/
+
+
+str = "Hello aditya"
+
+# wordlist = list(str)
+wordlist = [ch for ch in str if ch != ' ']
+print(wordlist)
