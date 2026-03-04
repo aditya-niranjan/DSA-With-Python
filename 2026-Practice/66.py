e5d389725498756a5110cf6af93596b6dfaaaabc
aditya-niranjan <adityaniranjan57@gmail.com>
2026-03-04 21:46:21 +0530

    solved 66 and 36

---
+++ 2026-Practice/66.py
@@ -0,0 +1,44 @@
+
+
+
+
+
+# digits = [9, 9]
+
+# # Convert list to string
+# dig = "".join(str(x) for x in digits)
+
+# # Add 1
+# add = int(dig) + 1
+
+# # Convert back to list of digits
+# result = [int(x) for x in str(add)]
+
+# print(result)
+
+
+
+# orrrrrrrrrr
+
+
+
+
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        # Traverse from right to left
+        for i in range(len(digits) - 1, -1, -1):
+            
+            # If digit is less than 9 → just increment and return
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            
+            # If digit is 9 → make it 0 and carry to next position
+            digits[i] = 0
+        
+        # If all digits were 9 (e.g., 9,9,9)
+        return [1] + digits
