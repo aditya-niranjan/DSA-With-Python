9f1b58ef208d5adf9bb5ec5e3034bbca144a27b7
aditya-niranjan <adityaniranjan57@gmail.com>
2026-03-04 23:00:45 +0530

    BinarySearch Revise

--- 2026-Practice/bianrySearch.py
+++ 2026-Practice/bianrySearch.py
@@ -1,3 +1,23 @@
 
-def binarySearch():
-    pass
+def binarySearch(arr,target):
+  left = 0
+  right= len(arr)-1
+
+  while left<right:
+    mid = (left+right)//2
+    if arr[mid] == target:
+        return mid
+    elif arr[mid]<=target:
+        left = mid+1
+    else:
+       right=mid
+  return left
+
+
+
+arr= [1,2,3,4,5,6,7,8,9,10]
+target = 1
+
+result = binarySearch(arr,target)
+
+print(result)
